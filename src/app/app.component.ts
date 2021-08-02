4acb9257e1488436cdd426935cec8d7386a369df
Balachandar.S <balachandar.s@niit.com>
2021-08-02 15:24:43 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'parent-child';
+
+
+  greet(name:string){
+    alert("Hi "+name);
+  }
 }
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  @Output() greetEvent = new EventEmitter();
+
+  name= "Stackroute";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  callParentGreet(){
+   this.greetEvent.emit(this.name);
+  }
+
+}
